3e6d0ad8e9a740402dc445c29d5b28913f2c8507
= <samyrameshcbe@gmail.com>
2026-02-04 21:38:21 +0530

    commiting the code

---
+++ src/org/sample/Company.java
@@ -0,0 +1,16 @@
+package org.sample;
+
+public class Company {
+	private void compnayName() {
+		System.out.println("Greens");
+	}
+	private void companyID() {
+		System.out.println(895785);
+
+	}
+	public static void main(String[] args) {
+		Company c=new Company();
+		c.companyID();
+		c.compnayName();
+	}
+}
